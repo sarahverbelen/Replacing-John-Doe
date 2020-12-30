a06ec716228499ed784ca3e55e656c45ef6e419b
sarahverbelen <sarah.verbelen@student.ehb.be>
2020-12-30 12:41:20 +0100

    (feat) initialising the tables

--- api/src/server.js
+++ api/src/server.js
@@ -19,7 +19,51 @@ app.get('/test', (req, res) => {
     res.status(200).send();
 });
 
+// connection to the database
+const pg = require('knex')({
+    client: 'pg',
+    version: '9.6',      
+    searchPath: ['knex', 'public'],
+    connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : 'postgres://example:example@localhost:5433/test'
+  });
 
+
+// initializing database tables (in case they don't exist yet)
+async function initialiseTables() {
+    await pg.schema.hasTable('placeholderData').then(async (exists) => {
+      if (!exists) {
+        await pg.schema
+          .createTable('placeholderData', (table) => {
+            table.increments();
+            table.uuid('uuid');
+            table.string('data');
+            table.integer('type');
+            table.integer('order');
+            table.timestamps(true, true);
+          })
+          .then(async () => {
+            console.log('created table placeholderData');
+          });
+  
+      }
+    });
+
+    
+    await pg.schema.hasTable('type').then(async (exists) => {
+      if (!exists) {
+        await pg.schema
+          .createTable('type', (table) => {
+            table.increments();
+            table.uuid('uuid');
+            table.string('type');
+            table.integer('typeID');
+          })
+          
+          
+      }
+    });
+  }
+  initialiseTables()
 
 
 module.exports = app;
