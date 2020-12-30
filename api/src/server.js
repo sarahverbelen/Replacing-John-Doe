d92c6f45055a03629d50e651bcee08ddfe3d33e8
sarahverbelen <sarah.verbelen@student.ehb.be>
2020-12-30 12:40:50 +0100

    (feat) adding uuid helper

--- api/src/server.js
+++ api/src/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 
+const Helpers = require('./utils/helpers.js')
+
 const app = express();
 http.Server(app);
 
@@ -16,5 +18,8 @@ app.get('/test', (req, res) => {
     console.log("test");
     res.status(200).send();
 });
+
+
+
 
 module.exports = app;
