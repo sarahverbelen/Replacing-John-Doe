8ee3770092028c0265a6ee6cc5e5c6d1b015a2aa
sarahverbelen <sarah.verbelen@student.ehb.be>
2020-12-30 16:15:23 +0100

    (fix) fixed problem with next() placement in tests

--- api/src/__tests__/endpoints.test.js
+++ api/src/__tests__/endpoints.test.js
@@ -7,7 +7,8 @@ describe('GET the test endpoint', () => {
     test('/test should respond with statuscode 200', async (next) => {
         try {
             const response = await request.get('/test');
-            expect(response.status).toBe(200, next());
+            expect(response.status).toBe(200);
+            next();
         } catch (e) {}
     });
 });
@@ -16,23 +17,28 @@ describe('create placeholder data', () => {
     test('POST request /create cannot be empty', async (next) => {
         try {
             const response = await request.post('/create');
-            expect(response.status).toBe(400, next());
-        } catch (e) {}
+            expect(response.status).toBe(400);
+            next();
+        } catch (e) {
+            // console.log(e);
+        }
     });
 
     test('POST request /create has to have data and a type', async (next) => {
         try {
             // only data
             const response1 = await request.post('/create').send({'data': 'test1234'});
-            expect(response1.status).toBe(400, next());
+            expect(response1.status).toBe(400);
 
             // only type
             const response2 = await request.post('/create').send({'type': 2});
-            expect(response2.status).toBe(400, next());
+            expect(response2.status).toBe(400);
 
             // both type and data
             const response3 = await request.post('/create').send({'data': 'test1234', 'type': 2});
-            expect(response3.status).toBe(200, next());
+            expect(response3.status).toBe(200);
+
+            next();
         } catch (e) {}
     });
 
@@ -40,11 +46,13 @@ describe('create placeholder data', () => {
         try {
             // number
             const response1 = await request.post('/create').send({'data': 123, 'type': 2});
-            expect(response1.status).toBe(400, next());
+            expect(response1.status).toBe(400);
 
             // string
             const response2 = await request.post('/create').send({'data': 'hello', 'type': 2});
-            expect(response2.status).toBe(200, next());
+            expect(response2.status).toBe(200);
+
+            next();
         } catch (e) {}
     });
 
@@ -52,15 +60,17 @@ describe('create placeholder data', () => {
         try {
             // string
             const response1 = await request.post('/create').send({'data': 'hello', 'type': '2'});
-            expect(response1.status).toBe(400, next());
+            expect(response1.status).toBe(400);
 
             // not 0, 1 or 2
             const response2 = await request.post('/create').send({'data': 'hello', 'type': 5});
-            expect(response2.status).toBe(400, next());
+            expect(response2.status).toBe(400);
 
             // number and 0, 1 or 2
             const response3 = await request.post('/create').send({'data': 'hello', 'type': 2});
-            expect(response3.status).toBe(200, next());
+            expect(response3.status).toBe(200);
+
+            next();
         } catch (e) {}
     });
 });
