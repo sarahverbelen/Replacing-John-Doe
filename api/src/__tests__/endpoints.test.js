134f54dbe4e5226dca17243dac94a2da29948db1
sarahverbelen <sarah.verbelen@student.ehb.be>
2020-12-30 15:16:11 +0100

    (test) created tests for endpoint /create

--- api/src/__tests__/endpoints.test.js
+++ api/src/__tests__/endpoints.test.js
@@ -1,5 +1,3 @@
-const { timeStamp } = require('console');
-const { hasUncaughtExceptionCaptureCallback } = require('process');
 const supertest = require('supertest');
 const app = require('./../server.js');
 
@@ -10,6 +8,59 @@ describe('GET the test endpoint', () => {
         try {
             const response = await request.get('/test');
             expect(response.status).toBe(200, next());
+        } catch (e) {}
+    });
+});
+
+describe('create placeholder data', () => {
+    test('POST request /create cannot be empty', async (next) => {
+        try {
+            const response = await request.post('/create');
+            expect(response.status).toBe(400, next());
+        } catch (e) {}
+    });
+
+    test('POST request /create has to have data and a type', async (next) => {
+        try {
+            // only data
+            const response1 = await request.post('/create').send({'data': 'test1234'});
+            expect(response1.status).toBe(400, next());
+
+            // only type
+            const response2 = await request.post('/create').send({'type': 2});
+            expect(response2.status).toBe(400, next());
+
+            // both type and data
+            const response3 = await request.post('/create').send({'data': 'test1234', 'type': 2});
+            expect(response3.status).toBe(200, next());
+        } catch (e) {}
+    });
+
+    test('POST request /create: the parameter -data- has to be of type string', async (next) => {
+        try {
+            // number
+            const response1 = await request.post('/create').send({'data': 123, 'type': 2});
+            expect(response1.status).toBe(400, next());
+
+            // string
+            const response2 = await request.post('/create').send({'data': 'hello', 'type': 2});
+            expect(response2.status).toBe(200, next());
+        } catch (e) {}
+    });
+
+    test('POST request /create: the parameter -type- has to be of type number and has to be either 0, 1 or 2', async (next) => {
+        try {
+            // string
+            const response1 = await request.post('/create').send({'data': 'hello', 'type': '2'});
+            expect(response1.status).toBe(400, next());
+
+            // not 0, 1 or 2
+            const response2 = await request.post('/create').send({'data': 'hello', 'type': 5});
+            expect(response2.status).toBe(400, next());
+
+            // number and 0, 1 or 2
+            const response3 = await request.post('/create').send({'data': 'hello', 'type': 2});
+            expect(response3.status).toBe(200, next());
         } catch (e) {}
     });
 });
