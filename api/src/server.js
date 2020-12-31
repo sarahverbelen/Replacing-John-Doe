6d5f459be07c6481fd3affebf617ded4cbca8045
sarahverbelen <sarah.verbelen@student.ehb.be>
2020-12-31 15:02:10 +0100

    feat: /create endpoint for adding new data

--- api/src/server.js
+++ api/src/server.js
@@ -9,86 +9,135 @@ http.Server(app);
 
 app.use(bodyParser.json());
 app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
+  bodyParser.urlencoded({
+    extended: true
+  })
 );
 
+// test endpoint
 app.get('/test', (req, res) => {
-    console.log("test");
-    res.status(200).send();
+  console.log("test");
+  res.status(200).send();
 });
+
+// create endpoint
+app.post('/create/:type/:data', (req, res) => {
+  // check if the req has a type and data
+  if (req.params.type != null && req.params.data != null) {
+    if (req.params.type == 0 || req.params.type == 1 || req.params.type == 2) {
+      // save the new data to the database
+      const uuid = Helpers.generateUUID(); // first we generate a uuid for this placeholder data
+
+      await pg.table('placeholderData').insert({
+        uuid,
+        data: req.params.data,
+        typeID: req.params.type
+      });
+
+    // send a statuscode when everything went right
+    res.status(200).send();
+  }
+}
+// send an error in case the parameters are not correct / empty
+res.status(400).send();
+});
+
+// overloading the create endpoint: in case there are not enough parameters given
+app.post('/create/:type', (req, res) => {
+  res.status(400).send();
+});
+
+app.post('/create', (req, res) => {
+  res.status(400).send();
+});
+
 
 // connection to the database
 const pg = require('knex')({
-    client: 'pg',
-    version: '9.6',      
-    searchPath: ['knex', 'public'],
-    connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : 'postgres://example:example@localhost:5432/test'
-  });
+  client: 'pg',
+  version: '9.6',
+  searchPath: ['knex', 'public'],
+  connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : 'postgres://example:example@localhost:5432/test'
+});
 
 
 // initializing database tables (in case they don't exist yet)
 async function initialiseTables() {
-    await pg.schema.hasTable('placeholderData').then(async (exists) => {
-      if (!exists) {
-        await pg.schema
-          .createTable('placeholderData', (table) => {
-            table.increments();
-            table.uuid('uuid');
-            table.string('data');
-            table.integer('typeID');
-            table.timestamps(true, true);
-          })
-          .then(async () => {
-            console.log('created table placeholderData');
-            // make a few titles, texts and names
+  await pg.schema.hasTable('placeholderData').then(async (exists) => {
+    if (!exists) {
+      await pg.schema
+        .createTable('placeholderData', (table) => {
+          table.increments();
+          table.uuid('uuid');
+          table.string('data');
+          table.integer('typeID');
+          table.timestamps(true, true);
+        })
+        .then(async () => {
+          console.log('created table placeholderData');
+          // make a few titles, texts and names
 
-            let listOfNames = ["John Doe", "Jane Doe", "Mark Verstappen", "Lily Verhaegen"];
-            let listOfTitles = ["Lorem Ipsum Dolor Sit Amet", "In Het Hulst Van De Nacht", "Mijn Meest Dierbare Herinneringen"];
-            let listOfTexts = ["I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.", "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.", "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind."];
-            for (let i = 0; i < listOfNames.length; i++) {
-                const uuid = Helpers.generateUUID();
-                await pg.table('placeholderData').insert({ uuid, data: listOfNames[i], typeID: 1 })
-              }
+          let listOfNames = ["John Doe", "Jane Doe", "Mark Verstappen", "Lily Verhaegen"];
+          let listOfTitles = ["Lorem Ipsum Dolor Sit Amet", "In Het Hulst Van De Nacht", "Mijn Meest Dierbare Herinneringen"];
+          let listOfTexts = ["I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.", "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.", "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind."];
+          for (let i = 0; i < listOfNames.length; i++) {
+            const uuid = Helpers.generateUUID();
+            await pg.table('placeholderData').insert({
+              uuid,
+              data: listOfNames[i],
+              typeID: 1
+            })
+          }
 
-              for (let i = 0; i < listOfTitles.length; i++) {
-                const uuid = Helpers.generateUUID();
-                await pg.table('placeholderData').insert({ uuid, data: listOfTitles[i], typeID: 0 })
-              }
+          for (let i = 0; i < listOfTitles.length; i++) {
+            const uuid = Helpers.generateUUID();
+            await pg.table('placeholderData').insert({
+              uuid,
+              data: listOfTitles[i],
+              typeID: 0
+            })
+          }
 
-              for (let i = 0; i < listOfTexts.length; i++) {
-                const uuid = Helpers.generateUUID();
-                await pg.table('placeholderData').insert({ uuid, data: listOfTexts[i], typeID: 2 })
-              }
-          });
-  
-      }
-    });
+          for (let i = 0; i < listOfTexts.length; i++) {
+            const uuid = Helpers.generateUUID();
+            await pg.table('placeholderData').insert({
+              uuid,
+              data: listOfTexts[i],
+              typeID: 2
+            })
+          }
+        });
 
-    let listOfTypes = ["title", "name", "text"];
-    
-    await pg.schema.hasTable('type').then(async (exists) => {
-      if (!exists) {
-        await pg.schema
-          .createTable('type', (table) => {
-            table.increments();
-            table.uuid('uuid');
-            table.string('type');
-            table.integer('typeID');
-          }).then(async () => {
-            console.log('created types');
-            for (let i = 0; i < listOfTypes.length; i++) {
-              const uuid = Helpers.generateUUID();
-              await pg.table('type').insert({ uuid, type: listOfTypes[i], typeID: i })
-            }
-          });
-          
-          
-      }
-    });
-  }
-  initialiseTables()
+    }
+  });
+
+  let listOfTypes = ["title", "name", "text"];
+
+  await pg.schema.hasTable('type').then(async (exists) => {
+    if (!exists) {
+      await pg.schema
+        .createTable('type', (table) => {
+          table.increments();
+          table.uuid('uuid');
+          table.string('type');
+          table.integer('typeID');
+        }).then(async () => {
+          console.log('created types');
+          for (let i = 0; i < listOfTypes.length; i++) {
+            const uuid = Helpers.generateUUID();
+            await pg.table('type').insert({
+              uuid,
+              type: listOfTypes[i],
+              typeID: i
+            })
+          }
+        });
+
+
+    }
+  });
+}
+initialiseTables()
 
 
 module.exports = app;
