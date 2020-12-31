c8ad986e328703162b019ff17ee56bb9e92e1073
sarahverbelen <sarah.verbelen@student.ehb.be>
2020-12-31 15:38:28 +0100

    # This is a combination of 7 commits. # This is the 1st commit message:
    (test) deleted temporary test
    
    (fix)
    
    # This is the commit message #2:
    
    (fix) cicd pipeline: first test fails
    
    # This is the commit message #3:
    
    (fix)
    
    # This is the commit message #4:
    
    (fix) first request in test didn't work
    
    # This is the commit message #5:
    
    (fix)
    
    # This is the commit message #6:
    
    (fix)
    
    # This is the commit message #7:
    
    (fix)

--- api/src/__tests__/endpoints.test.js
+++ api/src/__tests__/endpoints.test.js
@@ -3,6 +3,18 @@ const app = require('./../server.js');
 
 const request = supertest(app);
 
+
+describe('GET the test endpoint', () => {
+    test('/test should respond with statuscode 200', async (next) => {
+        try {
+            const response = await request.get('/test');
+            expect(response.status).toBe(200);
+            next();
+        } catch (e) {}
+    });
+});
+
+
 describe('GET the test endpoint', () => {
     test('/test should respond with statuscode 200', async (next) => {
         try {
